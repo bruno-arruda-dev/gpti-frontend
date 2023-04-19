8c0392171163c83e4057ad901ee0e6d74a133bfe
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-04-19 17:05:34 -0300

    Inicio da configuração da barra lateral

--- src/components/BarraLateral/BarraLateral.jsx
+++ src/components/BarraLateral/BarraLateral.jsx
@@ -1,9 +1,23 @@
 import './BarraLateral.scss';
+import Submenu from './Submenu/Submenu';
 
 function BarraLateral() {
   return (
     <div className='barraLateral'>
-      
+      <div className='barraLateral-cabecalho'>
+
+      </div>
+      <div className='barraLateral-conteudo'>
+        <Submenu texto="Cadastro" />
+        <Submenu texto="Venda" />
+        <Submenu texto="Produto" />
+        <Submenu texto="Financeiro" />
+        <Submenu texto="Relatório" />
+        <Submenu texto="Sistema" />
+      </div>
+      <div className='barraLateral-rodape'>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/BarraLateral/Submenu/Submenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Submenu({texto, icone}) {
+  return (
+    <div className='submenu'>
+      <h4>{texto}</h4>
+    </div>
+  );
+}
+
+export default Submenu;
