7a9215b9ef9f7e03e20aa24159e669d49cf01961
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-04-19 22:12:25 -0300

    Link na página de login para visualizar as demais paginas

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
   return (
     <div className='login'>
-      Login
+      <Link to="/Principal">
+        Aqui será desenvolvida a página de LOGIN. Enquanto isso, clique aqui para visualizar as outras páginas.
+      </Link>
     </div>
   );
 }
